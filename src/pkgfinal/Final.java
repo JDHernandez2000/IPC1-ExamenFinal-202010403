603be2fde960d241a727c58666d5a8f0a43e5369
Jose Hernández <3286881851101@ingenieria.usac.edu.gt>
2022-04-29 21:43:28 -0600

    Primeros dos problemas resueltos

--- src/pkgfinal/Final.java
+++ src/pkgfinal/Final.java
@@ -6,6 +6,7 @@ package pkgfinal;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Scanner;
 
 /**
  *
@@ -25,9 +26,8 @@ public class Final {
         });
      */
     public static menu men;
-    public static Problema1 p1 = new Problema1();
-    public static Problema2 p2 = new Problema2();
-    public static Problema3 p3 = new Problema3();
+    public static Scanner scan = new Scanner(System.in);
+   
     public static void main(String[] args) {
         men = new menu();
         men.show();
@@ -36,8 +36,15 @@ public class Final {
              @Override
              public void actionPerformed(ActionEvent actionEvent)
              {
-                 men.show(false);
-                 p1.show();
+                int n1, n2;
+                System.out.println("Ingrese el primer numero");
+                n1 = scan.nextInt();
+                System.out.println("Ingrese el segundo numero");
+                n2 = scan.nextInt();
+                if(n1 > n2){System.out.println("El numero mayor es " + String.valueOf(n1));}
+                else if(n1 < n2){System.out.println("El numero mayor es " + String.valueOf(n2));}
+                else{System.out.println("Son iguales");}
+                
              }
          });
         men.jButton2.addActionListener(new ActionListener()
@@ -45,8 +52,50 @@ public class Final {
              @Override
              public void actionPerformed(ActionEvent actionEvent)
              {
-                 men.show(false);
-                 p2.show();
+                int impar;
+                boolean bandera = true;
+                System.out.println("Ingrese el numero impar de la altura de la piramide");
+                impar = scan.nextInt();
+                if(impar % 2 == 0)
+                {
+                    bandera = false;
+                    System.out.println("Es un numero par" + String.valueOf(impar));
+                }
+                if(bandera == true)
+                {
+                    String str;
+                    for(int i = 1; i <= impar; i+=2)
+                    {
+                        int e = impar - i;
+                        String esp1 = "";
+                        String esp2 = "";
+                        String aste = "";
+                        for(int j = 0; j < e/2; j++)
+                        {
+                            esp1 += " ";
+                            esp2 += " ";
+                        }
+                        for (int h = 0; h < i; h++)
+                        {
+                            aste += "*";
+                        }
+                        if(i != 1)
+                        {
+                            str = "";
+                            str += esp1;
+                            str += aste;
+                            str += esp2;
+                            System.out.println(str);
+                        }
+                        else {
+                            str = "";
+                            str += esp1;
+                            str += "*";
+                            str += esp2;
+                            System.out.println(str);
+                        }
+                    }
+                }
              }
          });
         men.jButton3.addActionListener(new ActionListener()
@@ -54,36 +103,8 @@ public class Final {
              @Override
              public void actionPerformed(ActionEvent actionEvent)
              {
-                 men.show(false);
-                 p3.show();
              }
          });
-        p1.jButton1.addActionListener(new ActionListener()
-        {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent)
-            {
-                p1.show(false);
-                men.show();
-            }
-        });
-        p2.jButton1.addActionListener(new ActionListener()
-        {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent)
-            {
-                p2.show(false);
-                men.show();
-            }
-        });
-        p3.jButton1.addActionListener(new ActionListener()
-        {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent)
-            {
-                p3.show(false);
-                men.show();
-            }
-        });
+      
     }
 }
