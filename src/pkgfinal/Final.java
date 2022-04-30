37d8c9dda6728937f41b0d977286e9e540b74c79
JDHernandez2000 <3286881851101@ingenieria.usac.edu.gt>
2022-04-29 22:58:01 -0600

    Update Final.java

--- src/pkgfinal/Final.java
+++ src/pkgfinal/Final.java
@@ -151,7 +151,6 @@ public class Final {
         for (int i = 0; i < n - 1; i++)
             for (int j = 0; j < n - i - 1; j++)
                 if (arr[j].leche > arr[j + 1].leche) {
-                    // swap arr[j+1] and arr[j]
                     Vacas temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
